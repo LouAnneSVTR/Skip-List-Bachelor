61d4e0d02902eb8c5d53b3a737f4ae2fca202883
Exa <sofiane.couedel@gmail.com>
2020-11-03 16:22:26 +0100

    véréfication

--- src/SkipList.java
+++ src/SkipList.java
@@ -1,10 +1,30 @@
 public class SkipList {
-    private SkipListBound inSkipList;
-    private SkipListBound outSkipList;
+    private SkipListElementInt inSkipList;
+    private SkipListElementInt outSkipList;
+
+    private int heightMax;
 
     public SkipList() {
-        this.inSkipList     = new SkipListBound();
-        this.outSkipList    = new SkipListBound();
+        this.inSkipList     = new SkipListElementInt();
+        this.outSkipList    = new SkipListElementInt();
+
+        this.heightMax      = 1;
     }
 
+    public SkipListElementInt searchElementInt(SkipListElementInt inSkipList, int element) {
+
+        if (inSkipList == null) {
+            return null;
+        } else if (element != inSkipList.getElement()) {
+            if (inSkipList.getRight().getElement() < element) {
+                return searchElementInt(inSkipList.getRight(), element);
+            } else if (inSkipList.getBottom() != null) {
+                return searchElementInt(inSkipList.getBottom(), element);
+            } else {
+                return inSkipList;
+            }
+        } else {
+            return inSkipList;
+        }
+    }
 }
--- src/SkipListElementInt.java
+++ src/SkipListElementInt.java
@@ -1,16 +1,25 @@
 import java.util.Random;
 
-public class SkipListElementInt extends SkipListBound{
-    private final int element;
+public class SkipListElementInt{
+    private int element;
     private int height;
+
+    private boolean bound;
+
+    private SkipListElementInt top, right, bottom, left;
 
 
 
     public SkipListElementInt(int element) {
-        super();
         this.element = element;
         this.height = this.calculateHeight();
 
+        this.bound  = false;
+    }
+
+    public SkipListElementInt() {
+        this.bound = true;
+        this.element = 0;
     }
 
     //------------------------ ACCESSORS
@@ -18,8 +27,41 @@ public class SkipListElementInt extends SkipListBound{
         return this.element;
     }
 
+    public SkipListElementInt getTop() {
+        return this.top;
+    }
 
+    public void setTop(SkipListElementInt top) {
+        this.top = top;
+    }
 
+    public SkipListElementInt getRight() {
+        return this.right;
+    }
+
+    public void setRight(SkipListElementInt right) {
+        this.right = right;
+    }
+
+    public SkipListElementInt getBottom() {
+        return this.bottom;
+    }
+
+    public void setBottom(SkipListElementInt bottom) {
+        this.bottom = bottom;
+    }
+
+    public SkipListElementInt getLeft() {
+        return this.left;
+    }
+
+    public void setLeft(SkipListElementInt left) {
+        this.left = left;
+    }
+
+    public boolean isBound() {
+        return bound;
+    }
 
     //------------------------ METHODS
     public int calculateHeight() {
