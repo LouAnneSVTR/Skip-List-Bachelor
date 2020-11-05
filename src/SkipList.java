2c6b93df08092827f9c487b555ad9a2a86614e67
Exa <sofiane.couedel@gmail.com>
2020-11-05 16:12:58 +0100

    fonction concatenation

--- src/SkipList.java
+++ src/SkipList.java
@@ -59,11 +59,53 @@ public class SkipList {
         }
     }
 
+    public void concatenationSkipList(SkipList secondSkipList) {
+        SkipListInt skiplist1Upper = this.upper;
+        SkipListInt skipList2Lower = getLevelZeroElement(secondSkipList.getLower());
+
+        concatenationSkipList(skiplist1Upper, skipList2Lower);
+    }
+
+    public void concatenationSkipList(SkipListInt skiplist1Upper, SkipListInt skipList2Lower) {
+
+        if (skiplist1Upper.getTop() != null && skipList2Lower.getTop() != null) {
+            SkipListInt previousUpperSkipList1 = skiplist1Upper.getLeft();
+            SkipListInt nextLowerSkipList2 = skipList2Lower.getRight();
+
+            previousUpperSkipList1.setRight(nextLowerSkipList2);
+            nextLowerSkipList2.setLeft(previousUpperSkipList1);
+
+            concatenationSkipList(skiplist1Upper.getTop(), skipList2Lower.getTop());
+        } else if (skiplist1Upper.getTop() != null) {
+
+        } else if (skipList2Lower.getTop() != null) {
+
+        } else {
+            SkipListInt previousUpperSkipList1 = skiplist1Upper.getLeft();
+            SkipListInt nextLowerSkipList2 = skipList2Lower.getRight();
+
+            previousUpperSkipList1.setRight(nextLowerSkipList2);
+            nextLowerSkipList2.setLeft(previousUpperSkipList1);
+        }
+    }
+
     public boolean isEmpty() {
         return this.numberElements == 0;
     }
 
     public SkipListInt getLower() {
         return lower;
+    }
+
+    public SkipListInt getUpper() {
+        return upper;
+    }
+
+    public void setLowerRight(SkipListInt upper) {
+        this.lower.setRight(upper);
+    }
+
+    public void setUpperLeft(SkipListInt lower) {
+        this.upper.setLeft(lower);
     }
 }
